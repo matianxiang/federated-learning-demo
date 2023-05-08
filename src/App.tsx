c101c2f6a1a78e891fefe4bd6006851aae49064c
matianxiang <2950208312@qq.com>
2023-05-08 19:41:04 +0800

    run eject

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-import FederatedLearning from '../src/components/FederatedLearning'
+import FederatedLearning from './Page/FederatedLearning'
+import StartPage from './Page/StartPage'
+import Fork_Github_PNG from '@/static/fork_github.png'
 
 function App() {
+  const [isNextPage, setIsNextPage] = useState<boolean>(false)
+
   return (
-    <div >
-      <FederatedLearning></FederatedLearning>
+    <div style={{ position: 'relative' }}>
+      <img src={Fork_Github_PNG} alt='Fork_GitHub' style={{ position: 'fixed', top: '0px', right: '0px', width: '100px', height: '100px' }} />
+      {!isNextPage ? <StartPage></StartPage> : <FederatedLearning></FederatedLearning>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Page/FederatedLearning/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+import { Row, Col, Card, Spin } from 'antd'
+
+
+
+ const FederatedLearning =()=>{ 
+ 
+  return (
+    <>
+    cccc
+    </>
+  ) 
+ }  
+ export default FederatedLearning
